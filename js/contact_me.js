7c8001cc4d33573aa807bf8b45adc8f70757c83d
danogentili <daniil.gentili.dg@gmail.com>
2015-09-26 19:00:39 +0200

    Small tweaks

--- js/contact_me.js
+++ js/contact_me.js
@@ -15,15 +15,17 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var domain = "1";
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://mail.daniil.it",
                 type: "POST",
                 data: {
+                    domain: domain,
                     name: name,
                     phone: phone,
                     email: email,
